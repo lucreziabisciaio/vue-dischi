c38f345ada720e8b198da0630aef9ce81890be69
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-01-31 23:24:33 +0100

    BONUS: added loader on setTimeout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <header-container />
-    <album-container :albumList="albumList" />
+    <album-container v-if="loader" :albumList="albumList" />
+    <loader-box v-else />
   </div>
 </template>
 
@@ -9,24 +10,30 @@
 import axios from 'axios'
 import HeaderContainer from './components/HeaderContainer.vue'
 import AlbumContainer from './components/AlbumContainer.vue'
+import LoaderBox from './components/LoaderBox.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderContainer,
     AlbumContainer,
+    LoaderBox,
 
   },
   data() {
     return {
       albumList: [],
+      loader: false,
 
     }
   },
   mounted() {
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
+    setTimeout( () => {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
       this.albumList = response.data.response
-    })
+      this.loader = true
+      })
+    },2000)
   }
 }
 </script>
