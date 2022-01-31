9d275613578513e3e8a229bbc670813f2233bc52
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-01-31 15:25:38 +0100

    created file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-container />
+    <album-container />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import HeaderContainer from './components/HeaderContainer.vue'
+import AlbumContainer from './components/AlbumContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderContainer,
+    AlbumContainer,
+
+  },
+  data() {
+    return {
+      albumList: [],
+
+    }
+  },
+  mounted() {
+    axios.get('https://api.sampleapis.com/rickandmorty/characters').then((response) => {
+      this.albumList = response.data
+    })
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/main.scss'
 </style>
---
+++ src/components/AlbumContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container-fluid">
+        <album-card />
+    </div>
+</template>
+
+<script>
+import AlbumCard from './AlbumCard.vue'
+
+export default {
+    components: {
+        AlbumCard,
+
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
