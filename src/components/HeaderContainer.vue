8b20093ed34f5702de035db9d4fd2140fc2eca9c
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-01-31 17:43:45 +0100

    added header logo

--- src/components/HeaderContainer.vue
+++ src/components/HeaderContainer.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="container-fluid">
-
+    <div class="header_box p-2">
+        <div class="spotify_logo">
+            <img class="w-100" src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="">
+        </div>
     </div>
 </template>
 
@@ -11,5 +13,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.header_box {
+    background-color: rgba(255, 255, 255, 0.1);
+}
 
+.spotify_logo {
+    width: 50px;
+}
 </style>
